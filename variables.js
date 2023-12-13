3f9457c11ccbd44bed66feac36b2e6f3e50fba9b
Abhinav Kumar <abhinavkumar2369@outlook.in>
2023-12-13 19:34:06 +0000

    studied

---
+++ variables.js
@@ -0,0 +1,29 @@
+
+let someNumber = 10
+console.log(someNumber)
+// 10
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+// 10
+console.log(typeof stringNumber)
+// string
+
+let value = 3
+let negValue  = -value
+console.log(negValue)
+
+console.log(1+'2')
+console.log('1'+2)
+console.log(1+2+'4')
+console.log('1'+2+3)
+
+console.log(true)   //num
+console.log(true+true)    //num
+console.log("")     
+console.log("2"+"")   //string
+console.log(""+2)  // string
+
+let gameCounter = 1
+gameCounter++
+console.log(gameCounter)
