f3a482e6eb1ade4e6e7b9491f1b74c1036850a57
Abhinav Kumar <abhinavkumar2369@outlook.in>
2023-12-15 17:24:48 +0000

    Completed

---
+++ dates.js
@@ -0,0 +1,18 @@
+let myDate = new Date();
+console.log(myDate)
+console.log(myDate.toString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toJSON())
+
+
+
+
+let myCreatedDate = new Date("01-14-2023")
+console.log(myCreatedDate)
+console.log(myCreatedDate.toDateString())
+console.log(myCreatedDate.toLocaleString())
+console.log(myCreatedDate.getDate())
+console.log(myCreatedDate.getFullYear())
+console.log(myCreatedDate.getDay())
+console.log(myCreatedDate.getMonth())
+console.log(myCreatedDate)
