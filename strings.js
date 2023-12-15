7cdcde26daea8f306ae2fcedb4389b0d0ceaf64f
Abhinav Kumar <abhinavkumar2369@outlook.in>
2023-12-15 14:40:34 +0000

    Modified

--- strings.js
+++ strings.js
@@ -1,5 +1,45 @@
 const name = 'patel'
-const repoCount = 50
-console.log(name + repoCount + "value")
+// const repoCount = 50
+// console.log(name + repoCount + "value")
+// console.log(`Hello ${name}`)
 
 const gameName = new String("patel")
+console.log(name)
+console.log(typeof name)
+console.log(gameName)
+
+
+// console.log(gameName[1])
+// console.log(`${gameName}`)
+// console.log(typeof gameName)
+
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3))
+// Will return the element at first index if provided 
+
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = "  abhinav     "
+console.log(newStringOne)
+console.log(newStringOne.trimStart())
+
+let url = String("shaksham@gmail.com")
+console.log(url)
+console.log(typeof url)
+
+console.log(url.includes("@"))
+
+console.log(url.charAt(4))
+console.log(url.at(4))
+console.log(url.replace("@","--"))
+// only returns
+console.log(url)
