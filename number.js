e48774b3dd35d89ed0dfea6c2006e62b8177f6d4
Abhinav Kumar <abhinavkumar2369@outlook.in>
2023-12-15 15:34:20 +0000

    Completed

---
+++ number.js
@@ -0,0 +1,35 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(400)
+console.log(balance)
+
+console.log(typeof score)
+console.log(typeof balance)
+
+// balance = 400
+console.log(balance)
+
+console.log(score==balance)
+console.log(score===balance)
+
+const otherNumber = 123.456
+console.log(otherNumber.toPrecision(1))
+
+const hundreds = 1000000
+console.log(hundreds)
+
+console.log(hundreds.toLocaleString("en-IN"))
+
+
+console.log(Math.abs(-10))
+console.log(Math.floor(10.23))
+console.log(Math.round(12.45))
+
+console.log(Math.random())
+console.log((Math.random()*10).toPrecision(3))
+
+const min = 10
+const max = 20
+
+Math.random()
